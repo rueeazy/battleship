9e14d3056f6521fec9cd99ad984389b83c26793b
Christian Ruiz <santacruz.crm@gmail.com>
2020-10-08 21:26:39 -0700

    Updates

---
+++ precache-manifest.7a692a07d66fd5ce3b9de2a472135f12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb3e9fb148319046bf05ac49a0de6824",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "b30431973c0d907d3296",
+    "url": "/battleship/static/css/main.b64346c9.chunk.css"
+  },
+  {
+    "revision": "2a9f635f09c1f7ec60f8",
+    "url": "/battleship/static/js/2.2399fec1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship/static/js/2.2399fec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b30431973c0d907d3296",
+    "url": "/battleship/static/js/main.4443064c.chunk.js"
+  },
+  {
+    "revision": "1576b120f8ea920fbd40",
+    "url": "/battleship/static/js/runtime-main.9e463cc9.js"
+  }
+]);
